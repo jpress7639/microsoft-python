74a28d361bf8178e4367462db72fd5883434ab42
Jake Pressman <jpress7639@gmail.com>
2026-06-18 10:00:40 -0400

    matplotlib work

---
+++ course-2/module-3/common-visualizations.py
@@ -0,0 +1,57 @@
+# COMMON VISUALIZATIONS 
+
+# Pie chart 
+# Visual appeal of pie charts lie in their simplicity, 
+# provide an immediate sense of dominance 
+# have small categories 
+# less effective for small data segments 
+
+# Bar chart
+# excellent for comparing data across different groups
+# handle a range of data types 
+# they can be too bulky with too many categories 
+
+# Histograms
+# distribution of numerical data 
+# ideal for understanding frequency of data points 
+# reveal underlying patterns, skewness, or outliers 
+# sensitivity to bin width 
+
+# Line charts
+# display trends over time 
+# financial analysis, inventory levels, website traffic trends
+# can only be accurate in they're sparsed or irregularly spaced
+# Overlapping lines can also make it difficult without careful color management
+
+# Scatter plots 
+# plot data points on a horizontal and vertical axis to show the relationship between two variables
+# vast datasets may appear cluttered
+# Additional analysis may be needed if overlapping
+
+# Table visuals 
+# they organize information into rows and columns 
+# helpful when precision and detail is needed 
+# they can become overwhelming if there's too large datasets
+# ineffective to communicate trends 
+
+
+# REAL WORLD SCENARIO: ANALYZING USER ENGAGEMENT WITH A NEW FEATURE 
+
+# THE CHALLENGE: 
+# deploying a new product recommendation engine on your e-commerce site, 
+# you need to quickly understand if users are interacting with the new recommendations 
+# and if it's leading to more purchases
+
+# THE SOLUTION:
+# Using a Line Chart to Track Engagement Over Time
+# to visualize the daily click-through rates 
+# on the new recommendation engine over the past few weeks.
+
+# Why it's relevant: 
+# Visualizing the impact of these features allows you 
+# to quickly assess their success and identify areas for improvement.
+
+# X-axis: represents times (e.g. days or weeks since launch)
+# Y-axis: represents the click-through rate 
+
+# What you'd see: a clear line showing the trend
--- course-2/module-3/data-visualization-intro.py
+++ course-2/module-3/data-visualization-intro.py
@@ -1,3 +1,64 @@
 # DATA VISUALIZATION
 
 # What is Data Visualization?
+
+# about taking raw data, often trapped in the confines of spreadsheets or databases, 
+# and transforming it into visual representations 
+# that are both aesthetically pleasing and informative
+
+# A well-crafted chart or graph can instantly reveal trends, 
+# highlight outliers or unusual data points that deviate from the norm, 
+# and showcase patterns that might otherwise remain hidden within the raw data.
+
+# How are visualizations used?
+
+# Business: are indispensable for tracking key performance indicators (KPIs) that drive success.
+# Sales figures, customer satisfaction metrics, employee productivity levels, and market trends 
+
+# Science: rely on visualizations to unravel nature's mysteries
+# analyzing data from complex experiments, simulating natural phenomena, 
+# or visualizing intricate molecular structures, 
+# visualizations provide scientists with powerful tools 
+# for exploration, discovery, and communication
+
+# Healthcare: play a critical role in enhancing patient care, monitoring public health, 
+# and advancing medical research
+# to track patient health metrics, monitor the spread of diseases, 
+# evaluate the efficacy of treatments, and identify patterns in patient populations
+
+# Education: leverage visualizations to gain insights into student performance,
+# identify areas where students may be struggling, 
+# and tailor instructional strategies to meet the diverse needs of learners
+
+# Why are visualizations important?
+
+# They serve as a bridge between raw data and human understanding, 
+# allowing us to grasp complex information in ways that would be challenging
+
+# Unveiling patterns and trends: revealed within data that might 
+# not be readily apparent when examining raw numbers
+
+# Spotlighting outliers: those data points that significantly deviate from the norm
+# A box plot can display the distribution of data and clearly indicate outliers
+
+# Facilitating comparisons: can enable us to make effortless comparisons 
+# between different datasets
+
+# Enhancing communication: transcend language barriers 
+# and effectively communicate information to diverse audiences 
+
+# NOTE: should always approach with some dose of skepticism 
+
+# Importance of critical thinking in data visualization
+# holds immense power for illuminating insights and facilitating communication
+
+# NOTE: It's important to remember that visualizations, 
+# while often informative, can also be manipulated to mislead or even deceive
+
+# Cultivating critical thinking skills is essential when interpreting data visualizations. 
+# It's crucial to consider the source of the data, 
+# the methodologies employed in its collection and analysis, 
+# and any potential biases that might be present
+
+
+
